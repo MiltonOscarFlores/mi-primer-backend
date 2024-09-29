a2590188a3c57baa67822721b2bed5ecc007f24e
MiltonOscarFlores <miltonoscarflores@gmail.com>
2024-09-29 18:13:53 -0300

    separando la logica de servicios del routing

---
+++ services/product.js
@@ -0,0 +1,36 @@
+const { faker } = require('@faker-js/faker');
+
+class ProductsService {
+  constructor() {
+    this.products = [];
+    this.generate();
+  }
+
+  generate() {
+    const limit = 100;
+
+    for (let index = 0; index < limit; index++) {
+      this.products.push({
+        id: faker.string.uuid(),
+        name: faker.commerce.productName(),
+        price: Number(faker.commerce.price(), 10),
+        image: faker.image.urlPicsumPhotos(),
+      });
+    }
+  }
+  create() {}
+
+  find() {
+    return this.products;
+  }
+
+  findOne(id) {
+    return this.products.find((item) => item.id === id);
+  }
+
+  update() {}
+
+  delete() {}
+}
+
+module.exports = ProductsService;
